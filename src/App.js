e8d28b15bba9cc194110eebff9bf6005b4dfb18c
Akash Kriplani <aakash23kriplani@gmail.com>
2023-05-18 17:07:52 +0530

    Fetch the list of users from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -5,25 +5,21 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: '12e1231e'
-        },
-        {
-          name: 'Frank',
-          id: '12ed2acas'
-        },
-        {
-          name: 'Jacky',
-          id: '1231e'
-        },
-        {
-          name: 'Andrei',
-          id: '12e1e213'
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state)
+        )
+      );
   }
 
   render() {

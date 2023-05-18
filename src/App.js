7181cf0c6a729b28cb605fd1ef9be2c18b09e44f
Akash Kriplani <aakash23kriplani@gmail.com>
2023-05-18 16:26:13 +0530

    Add monsters array and map it over to show the list of monsters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Linda',
+          id: '12e1231e'
+        },
+        {
+          name: 'Frank',
+          id: '12ed2acas'
+        },
+        {
+          name: 'Jacky',
+          id: '1231e'
+        },
+        {
+          name: 'Andrei',
+          id: '12e1e213'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;

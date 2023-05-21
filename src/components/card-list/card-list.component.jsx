d650cf7c9632e7ad44ed40d431e7ed34f0bcbc54
Akash Kriplani <aakash23kriplani@gmail.com>
2023-05-21 22:45:16 +0530

    Create a card component and use it in a card list component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Card from '../card/card.component';
 import './card-list.styles.css';
 
 class CardList extends Component {
@@ -7,14 +8,7 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          const { name, email, id } = monster;
-          return (
-            <div key={id} className="card-container">
-              <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card key={monster.id} monster={monster} />;
         })}
       </div>
     );
